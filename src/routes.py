c60f7545e06225566d45639dcb6d8ff5db40e2b8
TM <gustavoplm@hotmail.com>
2021-10-06 16:29:23 -0300

    Base do login e página do login feitas

--- src/routes.py
+++ src/routes.py
@@ -3,18 +3,28 @@ from flask import Blueprint, render_template
 routes = Blueprint('routes', __name__)
 
 
-@routes.route('/')
+@routes.route('/home')
 def main():
     return render_template('home.html')
+
 
 @routes.route('/config')
 def config():
     return render_template('config.html')
 
+
 @routes.route('/editar')
 def edit():
     return render_template('editar.html')
 
+
 @routes.route('/arquivos')
 def archive():
     return render_template('arquivos.html')
+
+# Rotas login
+
+
+@routes.route('/')
+def login():
+    return render_template('login.html')
