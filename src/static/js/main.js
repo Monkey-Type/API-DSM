fa8cac2463302d3e1328275261fe748962698e4b
TM <gustavoplm@hotmail.com>
2021-10-06 11:17:56 -0300

    popup funcional

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,4 @@
-var menu = document.getElementById("menu");
+/* var menu = document.getElementById("menu");
 var hamburger = document.getElementById("nav");
 hamburger.addEventListener("click", () => {
   if (menu.classList.contains('toggled')) {
@@ -7,4 +7,19 @@ hamburger.addEventListener("click", () => {
   else {
     menu.classList.add('toggled')
   }
-});
+}); */
+
+// Popup
+var popup = document.getElementById("popup")
+var popupBg = document.getElementById("popupBg")
+var btnPopup = document.getElementById("btnpop")
+
+btnPopup.addEventListener("click", () => {
+  popup.classList.add('popup-active')
+  document.body.style.overflow = "hidden"
+})
+
+popupBg.addEventListener("click", () => {
+  popup.classList.remove('popup-active')
+  document.body.style.overflow = "auto"
+})
