bceac092376cdf8f190c063781387915e51f550c
TM <gustavoplm@hotmail.com>
2021-10-18 09:44:34 -0300

    Mensagens de validação arrumadas, estilização de inputs ao mostrar mensagem de erro feita e mascara de input adicionada para cpf e ra

--- src/app/formulario/registerForm.py
+++ src/app/formulario/registerForm.py
@@ -1,36 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, validators
-from wtforms.validators import InputRequired, Length, ValidationError, Email
+from wtforms.validators import EqualTo, InputRequired, Length, Regexp, ValidationError, Email
 from app import *
 # from flask_wtf.form import FlaskForm
 from wtforms.fields.html5 import EmailField
 
 
+message = 'Este campo é necessario'
+
+
 class RegisterForm(FlaskForm):
-    email = EmailField(validators=[InputRequired(), Email()], render_kw={
-                       "placeholder": "exemple@fatec.sv.gov.br"})
+    email = EmailField(validators=[InputRequired(message=message), Email(message='Digite um email valido')], render_kw={
+        "placeholder": "exemple@fatec.sv.gov.br"})
 
-    ra = StringField('RA / Matricula', validators=[InputRequired(), Length(
-        min=4, max=50, message="Digite um RA valido")], render_kw={"placeholder": "RA / Matricula"})
+    ra = StringField('RA / Matricula', validators=[InputRequired(message=message), Length(
+        min=13, max=13, message="Digite um RA valido")], render_kw={"placeholder": "RA / Matricula"})
 
-    cpf = StringField(validators=[InputRequired(), Length(
-        min=4, max=50, message="Digite um CPF valido")], render_kw={"placeholder": "Sem traços ou espaços"})
+    cpf = StringField(validators=[InputRequired(message=message), Regexp(
+        '^\d{3}\.\d{3}\.\d{3}\-\d{2}$', message='Digite um CPF')], render_kw={"placeholder": "Sem traços ou espaços"})
 
-    nome = StringField(validators=[InputRequired(),  Length(
-        min=5, max=70)], render_kw={
+    nome = StringField(validators=[InputRequired(message=message), Regexp("^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)$", message='Digite nome e sobrenome')], render_kw={
         "placeholder": "Nome completo"})
 
-    senha = PasswordField('Senha', [
-        validators.DataRequired(),
-        validators.EqualTo(
-            'confirm', message='Senha e corfimação não conferrem'),
-        validators.Length(
-            min=5, max=70, message="Senha deve ter pelomenos 5 caracteres")
-    ], render_kw={
-        "placeholder": "Mínimo de 5 caracteres"})
+    senha = PasswordField('Senha', validators=[InputRequired(message=message), Length(min=5, max=70, message="Senha deve ter pelomenos 5 caracteres"), EqualTo(
+        'confirm', message='Senha e corfimação não conferrem')], render_kw={"placeholder": "Mínimo de 5 caracteres"})
     confirm = PasswordField('Confirmar senha', render_kw={
         "placeholder": "Mínimo de 5 caracteres"},
-        validators=[InputRequired(), Length(min=5, max=70, message="Senha deve ter pelomenos 5 caracteres")])
+        validators=[InputRequired(message=message), Length(min=5, max=70, message="Senha deve ter pelomenos 5 caracteres"), EqualTo('senha', message='Senha e corfimação não conferrem')])
 
     '''def validaçãoUser(self, cpf):
         usuarioExistente = User.query.filter_by(cpf=cpf.data).first()
@@ -41,8 +37,8 @@ class RegisterForm(FlaskForm):
 
 
 class LoginFormulario(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Digite um email valido')], render_kw={
-                        "placeholder": "exemple@fatec.sp.gov.br"})
-    senha = PasswordField('Senha', validators=[InputRequired(), Length(
+    email = EmailField('Email', validators=[InputRequired(message=message), Email(message='Digite um email valido')], render_kw={
+        "placeholder": "exemple@fatec.sp.gov.br"})
+    senha = PasswordField('Senha', validators=[InputRequired(message=message), Length(
         min=2, max=100, message='Senha deve ter pelomenos 5 caracteres')], render_kw={
         "placeholder": "Mínimo de 5 caracteres"})
---
+++ src/app/static/js/mask.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $('#cpf').mask('000.000.000-00');
+  $('#ra').mask('0000000000000');
+});
