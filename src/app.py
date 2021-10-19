7ca76324f7c360c123952196c6f86f163b1488bf
TM <gustavoplm@hotmail.com>
2021-10-19 14:43:58 -0300

    Arrumei a criação do banco (agr está referenciado no app.py)

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from app import create_app
+from app import create_app, create_database
 
 app = create_app()
+db = create_database(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    db.run()
